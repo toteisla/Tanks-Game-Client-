81ab4908cb35b2c0ca72e8fadd4dcfdd9674aeb2
toteisla <cancelo.garcia@gmail.com>
2014-05-11 12:42:51 +0200

    + Añadida Clase utils + Añadida función gelAlpha

---
+++ Project1/Project1/utils.cpp
@@ -0,0 +1,11 @@
+#include <allegro5\allegro.h>
+#include <math.h>
+#include "utils.h"
+
+float getAlpha(int x1, int y1, int x2, int y2){
+    float alpha;
+    float aux = (x2 - x1) / (y2 - y1);
+    alpha = atan( aux );
+
+    return alpha;
+}
---
+++ Project1/Project1/utils.h
@@ -0,0 +1,11 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+class Utils {
+    private:
+
+    public :
+        float getAlpha(int x1, int y1, int x2, int y2);
+};
+
+#endif
